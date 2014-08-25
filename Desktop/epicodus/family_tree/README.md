a0b456b4a46d15a51eda21338d45f38e9ca14f72
dianedouglas <diane.douglas1@gmail.com>
2014-08-24 20:02:06 -0700

    Add file hierarchy. Include Rakefile, config.yml, gemfile and gemfile.lock, spec_helper, ui file with database connection and all requires.

---
+++ Desktop/epicodus/family_tree/README.md
@@ -0,0 +1,5 @@
+Family Tree
+
+By Diane Douglas
+
+**A command line program which creates a PSQL database of family members, organized into a family tree, controlled by ActiveRecord.**
---
+++ Desktop/epicodus/family_tree/spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'active_record'
+
+require 'bundler/setup'
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+database_configurations = YAML::load(File.open('./db/config.yml'))
+test_configuration = database_configurations['test']
+ActiveRecord::Base.establish_connection(test_configuration)
+
+RSpec.configure do |config|
+  config.after(:each) do
+  	#Class.all.each {|class| class.destroy}
+  end
+end
